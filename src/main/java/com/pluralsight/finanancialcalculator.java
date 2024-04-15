a14d904b40c7899621e51ed2fe68bea0861663ab
OtG5 <liloscar101@gmail.com>
2024-04-15 15:56:21 -0400

    hopefully my code is updated this time

--- src/main/java/com/pluralsight/finanancialcalculator.java
+++ src/main/java/com/pluralsight/finanancialcalculator.java
@@ -1,2 +1,63 @@
-package com.pluralsight;public class finanancialcalculator {
+package com.pluralsight;
+import java.util.Scanner;
+public class finanancialcalculator {
+    public static void main(String[] args) {
+        final int monthsINYear = 12;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+        System.out.print("Enter the principal amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.print("Enter the current interest rate: ");
+        double interestRate = scanner.nextDouble();
+
+        System.out.print("Enter the term in years: ");
+        int monthlyLoanlength = scanner.nextInt();
+
+        // monthly payment
+
+        double compoundInterst =  principal * math.pow(interestRate * (1 + interestRate));
+
+        (interestRate * Math.pow((1+interestRate),monthlyLoanlength))
+        /  (Math.pow(1+ interestRate, monthlyLoanlength));
+
+
+        System.out.printf("Total Monthly Payment: %2f", monthlyPayments);
+
+
+
+    }
+
+
+    /**
+     * the deposit data double
+     *
+     *
+     * else if (calcType == 2) {
+     *
+     *
+     *     System.out.println("This is your CD calculator");
+     *     System.out.print("");
+     *
+     *     double deposti = scanner.nextDouble();
+     *
+     *     System.out.print("please enter your interst rate: ");
+     *     double cdInterestRate = scannner.nextdouble(O;
+     *
+     *     System.out.print();
+     *
+     *     cdInterestRate /= 100;
+     * double futureValue = Math.pow((1 + InterestRate), yearsToMature);
+     * double endingBalance = deposit * futrureValue;
+     *
+     * double earnedInterest = endingBalance - deposit;
+     *
+     * System.out.printf("Our future value: %.2f with a earned interest of: %.2f, endingBalance , )
+     * }
+     * */
 }
